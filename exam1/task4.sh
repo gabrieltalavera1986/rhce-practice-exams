a3ba42606bb97ae96c9c90e1477893f67d628ad2
gabrieltalavera1986 <147014049+gabrieltalavera1986@users.noreply.github.com>
2024-09-13 18:09:09 -0400

    Update task4.sh to check for sudoers.d files

--- exam1/task4.sh
+++ exam1/task4.sh
@@ -35,9 +35,9 @@ for host in "${hosts[@]}"; do
         fi
     done
 
-    # Check if tony has sudo privileges without a password in the sudoers file
+    # Check if tony has sudo privileges without a password in sudoers file or sudoers.d directory
     echo "Checking sudo privileges for tony on $host..."
-    if ssh "$host" "sudo grep '^tony ALL=(ALL) NOPASSWD:ALL' /etc/sudoers" >/dev/null 2>&1; then
+    if ssh "$host" "sudo grep -r '^tony.*NOPASSWD:.*ALL' /etc/sudoers /etc/sudoers.d" >/dev/null 2>&1; then
         echo "User tony has sudo privileges without a password on $host."
     else
         echo "User tony does NOT have sudo privileges without a password on $host."
@@ -50,5 +50,6 @@ if $all_accomplished; then
     echo "This task has been completed successfully!"
     exit 0
 else
+    echo "This task has NOT been completed successfully."
     exit 1
 fi
